a0d84f03507c923d70a00a8846f7f8263b357f16
bgr204 <bgr204@exeter.ac.uk>
2024-04-06 08:52:29 +0100

    Tests for Normality

--- Models.R
+++ Models.R
@@ -30,10 +30,13 @@ library(sjPlot)
 library(ggplot2)
 library(EnvStats)
 library(forecast)
+library(moments)
+library(car)
 
 #-------------------------Formatting--------------------------------------------
 
 ###changing column formats
+disturbance$vigilance <- as.numeric(disturbance$vigilance)
 disturbance$human_rate <- as.numeric(disturbance$human_rate)
 disturbance$start_time <- chron(times=disturbance$start_time)
 disturbance$location_code <- as.factor(disturbance$location_code)
@@ -48,7 +51,7 @@ disturbance$wind_speed <- as.numeric(factor(disturbance$wind_speed), levels =
 
 #-------------------------Optimal Lambda Function-------------------------------
 
-find_optimal_lambda <- function(data) {
+lambda_transform <- function(data) {
   
   # Sequence of lambda values to test
   lambda_seq <- seq(-2, 2, by = 0.1)
@@ -71,12 +74,55 @@ find_optimal_lambda <- function(data) {
   optimal_lambda <- lambda_seq[which.min(abs(skewness_values))]
   
   # Apply Box-Cox transformation with optimal lambda
-  data <- BoxCox(data, optimal_lambda)
+  data <- as.numeric(BoxCox(data, optimal_lambda))
 
+}
+optimal_lambda <- function(data) {
+  
+  # Sequence of lambda values to test
+  lambda_seq <- seq(-2, 2, by = 0.1)
+  
+  # Function to compute skewness
+  compute_skewness <- function(x) {
+    mean((x - mean(x))^3) / (sd(x)^3)
+  }
+  
+  # Container for skewness values
+  skewness_values <- numeric(length(lambda_seq))
+  
+  # Perform Box-Cox transformation for each lambda value
+  for (i in seq_along(lambda_seq)) {
+    transformed_data <- BoxCox(data, lambda_seq[i])
+    skewness_values[i] <- compute_skewness(transformed_data)
+  }
+  
+  # Find the lambda value with the smallest skewness
+  lambda_seq[which.min(abs(skewness_values))]
+  
 }
 
 
 #-------------------------Disturbance Models------------------------------------
+
+###checking normality
+#histogram of vigilance
+hist(disturbance$vigilance, breaks = 30)
+#optimal lambda transformation
+disturbance$tf_vigilance <- lambda_transform(disturbance$vigilance+1)
+#optimal lambda value
+optimal_lambda(disturbance$vigilance)
+#histogram of transformed data
+hist(disturbance$tf_vigilance, breaks = 30)
+#checking no values below 0
+min(disturbance$tf_vigilance)
+#qqnorm plot
+qqnorm(disturbance$tf_vigilance)
+qqline(disturbance$tf_vigilance)
+#density plot
+plot(density(disturbance$vigilance))
+#test for normality
+jarque.test(disturbance$tf_vigilance)
+
 
 ###disturbance model for vigilance
 #without location
@@ -129,27 +175,52 @@ ggplot(disturbance, aes(x = human_rate, y = flight, colour = location_code))+
 
 #-------------------------Step Length Model-------------------------------------
 
+###formatting
 step_length$species <- as.factor(step_length$species)
 step_length$id <- as.factor(step_length$id)
 step_length$is_weekend <- as.factor(step_length$is_weekend)
 
+###checking normality
+#histogram of distance
+hist(step_length$distance, breaks = 30)
+#optimal lambda transformation
+step_length$tf_distance <- lambda_transform(step_length$distance+1)
+#optimal lambda value
+optimal_lambda(step_length$distance)
+#histogram of transformed data
+hist(step_length$tf_distance, breaks = 30)
+#checking no values below 0
+min(step_length$tf_distance)
+#qqnorm plot
+qqnorm(step_length$tf_distance)
+qqline(step_length$tf_distance)
+#density plot
+plot(density(step_length$distance))
+#test for normality
+jarque.test(step_length$tf_distance)
 
+###non parametric mixed effects model
+#with interaction
 step_m1 <- lmer(data = step_length, distance~is_weekend*species+(1|id))
+#non parametric type III anova
+Anova(step_m1, type = "III", test = "Chisq")
+#removed interaction
 step_m2 <- lmer(data = step_length, distance~is_weekend+species+(1|id))
-step_m3 <- update(step_m2,~.-is_weekend)
-anova(step_m1, step_m2, test = "F")
-anova(step_m2, step_m3, test = "F")
+#non parametric type III anova
+Anova(step_m2, type = "III", test = "Chisq")
+#summary
+summary(step_m2)
 
-summary(step_m1)
-
-# Create ggeffects object
-effect_step <- ggpredict(step_m1, terms = c("is_weekend", "species"))
-
-# Plot marginal effects
+###plotting with ggeffects
+#create ggeffects object
+effect_step <- ggpredict(step_m2, terms = c("is_weekend", "species"))
+#plot marginal effects
 plot(effect_step)
 
+###plotting with ggplot
+#generate predictions
 step_length$predicted <- predict(step_m2, newdata = step_length)
-
+#create plot
 ggplot(step_length, aes(x = is_weekend, y = predicted, colour = species)) +
   geom_boxplot() +  # Add actual data points
   stat_summary(fun = median, aes(group = species, color = species), geom = "point", shape = 18, size = 3, position = position_dodge(width = 0.75)) +  # Add median points
@@ -160,29 +231,53 @@ ggplot(step_length, aes(x = is_weekend, y = predicted, colour = species)) +
 
 #-------------------------Home Range Model--------------------------------------
 
+###formatting
 home_range$species <- as.factor(home_range$species)
 home_range$id <- as.factor(home_range$id)
 home_range$is_weekend <- as.factor(home_range$is_weekend)
 home_range$area <- home_range$area/1000000
 
-home_m1 <- lmer(data = home_range, area~is_weekend*species+(1|id))
-home_m2 <- lmer(data = home_range, area~is_weekend+species+(1|id))
-home_m3 <- update(home_m2,~.-is_weekend)
-anova(home_m1, home_m2, test = "F")
-anova(home_m2, home_m3, test = "F")
+###checking normality
+#histogram of area
+hist(home_range$area, breaks = 30)
+#optimal lambda transformation
+home_range$tf_area <- lambda_transform(home_range$area+1)
+#optimal lambda value
+optimal_lambda(home_range$area)
+#histogram of transformed data
+hist(home_range$tf_area, breaks = 30)
+#checking no values below 0
+min(home_range$tf_area)
+#qqnorm plot
+qqnorm(home_range$tf_area)
+qqline(home_range$tf_area)
+#density plot
+plot(density(home_range$tf_area))
+#test for normality
+jarque.test(home_range$tf_area)
 
+###non parametric mixed effects model
+#with interaction
+home_m1 <- lmer(data = home_range, tf_area~is_weekend*species+(1|id))
+#non parametric type III anova
+Anova(home_m1, type = "III", test = "Chisq")
+#removed interaction
+home_m2 <- lmer(data = home_range, tf_area~is_weekend+species+(1|id))
+#non parametric type III anova
+Anova(home_m2, type = "III", test = "Chisq")
+#summary
 summary(home_m1)
 
-# Create ggeffects object
-effect_home <- ggpredict(home_m1, terms = c("is_weekend", "species"))
-
-# Plot marginal effects
+###plotting with ggeffect
+#create ggeffects object
+effect_home <- ggpredict(home_m2, terms = c("is_weekend", "species"))
+#plot marginal effects
 plot(effect_home)
 
-# Generate predictions
+###plotting with ggplot
+#generate predictions
 home_range$predicted <- predict(home_m2, newdata = home_range)
-
-# Create plot
+#create plot
 ggplot(home_range, aes(x = is_weekend, y = predicted, colour = species)) +
   geom_boxplot() +  # Add actual data points
   stat_summary(fun = median, aes(group = species, color = species), geom = "point", shape = 18, size = 4, position = position_dodge(width = 0.75)) +  # Add median points
@@ -192,52 +287,61 @@ ggplot(home_range, aes(x = is_weekend, y = predicted, colour = species)) +
 
 #-------------------------Distance to Road Models-------------------------------
 
+###formatting
 #change format of date
 road_distance$UTC_date <- as.Date(road_distance$UTC_date)
 #add column with weekday
 road_distance$day <- weekdays(road_distance$UTC_date)
 #creating a column for weekend or not
 road_distance$is_weekend <- road_distance$day %in% c("Saturday", "Sunday")
-
+#as.factor
 road_distance$species <- as.factor(road_distance$species)
 road_distance$device_id <- as.factor(road_distance$device_id)
 road_distance$is_weekend <- as.factor(road_distance$is_weekend)
 road_distance$road <- as.factor(road_distance$road)
-road_distance$log_road <- sqrt(road_distance$distance)
 
-# Sequence of lambda values to test
-lambda_seq <- seq(-2, 2, by = 0.1)
+###checking normality
+#histogram of distance
+hist(road_distance$distance, breaks = 30)
+#optimal lambda transformation
+road_distance$tf_distance <- lambda_transform(road_distance$distance+1)
+#optimal lambda value
+optimal_lambda(road_distance$distance)
+#histogram of transformed data
+hist(road_distance$tf_distance, breaks = 30)
+#checking no values below 0
+min(road_distance$tf_distance)
+#qqnorm plot
+qqnorm(road_distance$tf_distance)
+qqline(road_distance$tf_distance)
+#density plot
+plot(density(road_distance$tf_distance))
+#test for normality
+jarque.test(road_distance$tf_distance)
 
-# Function to compute skewness
-compute_skewness <- function(x) {
-  mean((x - mean(x))^3) / (sd(x)^3)
-}
+###non parametric mixed effects model
+#with interaction
+road_m1 <- lmer(data = road_distance, tf_distance~is_weekend*species+(1|device_id))
+#non parametric type III anova
+Anova(road_m1, type = "III", test = "Chisq")
+#summary
+summary(road_m1)
 
-# Container for skewness values
-skewness_values <- numeric(length(lambda_seq))
+###plotting with ggeffect
+# Create ggeffects object
+effect_road <- ggpredict(road_m1, terms = c("is_weekend", "species"))
+# Plot marginal effects
+plot(effect_road)
 
-# Perform Box-Cox transformation for each lambda value
-for (i in seq_along(lambda_seq)) {
-  transformed_data <- BoxCox(road_distance$distance, lambda_seq[i])
-  skewness_values[i] <- compute_skewness(transformed_data)
-}
+###plotting with ggplot
+#generate predictions
+road_distance$predicted <- predict(road_m1, newdata = road_distance)
+#create plot
+ggplot(road_distance, aes(x = is_weekend, y = predicted, colour = species)) +
+  geom_boxplot() +  # Add actual data points
+  stat_summary(fun = median, aes(group = species, color = species), geom = "point", shape = 18, size = 4, position = position_dodge(width = 0.75)) +  # Add median points
+  stat_summary(fun = median, aes(group = species, color = species), geom = "line", linetype = "dashed", position = position_dodge(width = 0.75)) +  # Add lines between medians
+  labs(title = "Distance to Road", x = "Weekend", y = "Distance to Road (m)")
+                                                                         
 
-# Plot skewness values for different lambda values
-plot(lambda_seq, skewness_values, type = "l", xlab = "Lambda", ylab = "Skewness")
 
-# Find the lambda value with the smallest skewness
-optimal_lambda <- lambda_seq[which.min(abs(skewness_values))]
-cat("Optimal lambda:", optimal_lambda, "\n")
-
-# Apply Box-Cox transformation with optimal lambda
-road_distance$tf_distance <- BoxCox(road_distance$distance, optimal_lambda)
-
-# Plot histogram of transformed data
-hist(road_distance$tf_distance, breaks = 100)
-
-road_m1 <- lmer(data = road_distance, tf_distance~is_weekend*species+road+(1|device_id))
-road_m2 <- lmer(data = road_distance, tf_distance~is_weekend+species+road+(1|device_id))
-road_m3 <- update(road_m2,~.-is_weekend)
-anova()
-
-road_distance$tf_distance <- find_optimal_lambda(road_distance$distance)
